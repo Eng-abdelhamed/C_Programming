55fb4f80db5ba501f041118fc5792b177b884dbd
Eng-abdelhamed <168166326+Eng-abdelhamed@users.noreply.github.com>
2024-06-09 08:34:26 +0300

    Update main.cpp

--- queue/main.cpp
+++ queue/main.cpp
@@ -1,66 +1,108 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
-# define size 5
-int queue[size];
-int front =-1;
-int rear=-1;
 
-void enqueue(int value)
-{
-    if(rear != size -1)
-    {
-        if(rear == -1 && front == -1)
-        {
-            rear ++;
-            front ++;
-            queue[rear] =  value;
+class queue {
+private:
+    int rear;
+    int front;
+    int size;
+    int *items;
+
+public:
+    queue(int s) {
+        size = s;
+        front = rear = -1;
+        items = new int[size];
+    }
+
+    void enqueue(int value) {
+        if (isfull()) {
+            cout << "Queue is full\n";
+            return;
         }
-        else
-        {
-            queue[++rear]=value;
+        if (isempty()) {
+            front = rear = 0;
+        } else if (rear == size - 1 && front != 0) {
+            rear = 0;
+        } else {
+            rear++;
+        }
+        items[rear] = value;
+    }
+
+    bool isempty() {
+        return (front == -1);
+    }
+
+    bool isfull() {
+        return ((rear + 1) % size == front);
+    }
+
+    void dequeue() {
+        if (isempty()) {
+            cout << "Queue is empty\n";
+            return;
+        }
+        if (front == rear) {
+            front = rear = -1;
+        } else if (front == size - 1) {
+            front = 0;
+        } else {
+            front++;
         }
     }
-    else
-    {
-        cout<<"queue is full\n";
-    }
-}
-void dequeue()
-{
-    if(front != -1 && rear != -1 && front <= rear)
-    {
-        front ++;
-    }
-    else
-    {
-        cout<<"queue is empty \n";
-    }
-}
-int peek()
-{
-     if(front != -1 && rear != -1 && front <= rear)
-    {
-        return queue[front];
-    }
-    else
-    {
-        cout<<"queue is empty \n";
-        return -1;
-    }
-}
-void display()
-{
-    if(front != -1 && rear != -1 && front <= rear)
-    {
-        for(int i = front; i <= rear ;i++)
-        {
-            cout<<queue[i]<<" ";
+
+    int peak() {
+        if (isempty()) {
+            cout << "Queue is empty\n";
+            return -1;
+        } else {
+            return items[front];
         }
-        cout<<endl;
     }
-    else
-    {
-        cout<<"queue is empty \n";
+
+    void display() {
+        if (isempty()) {
+            cout << "Queue is empty\n";
+            return;
+        }
+        cout << "Queue elements are: ";
+        if (rear >= front) {
+            for (int i = front; i <= rear; i++) {
+                cout << items[i] << " ";
+            }
+        } else {
+            for (int i = front; i < size; i++) {
+                cout << items[i] << " ";
+            }
+            for (int i = 0; i <= rear; i++) {
+                cout << items[i] << " ";
+            }
+        }
+        cout << endl;
     }
+};
+
+int main() {
+    int numbers;
+    int size;
+    cout << "Enter the size of the queue: ";
+    cin >> size;
+
+    queue q1(size);
+
+    cout << "Enter the elements (0 to stop entering): ";
+    while (true) {
+        cin >> numbers;
+        if (numbers == 0 || q1.isfull()) break;
+        q1.enqueue(numbers);
+    }
+
+    q1.display();
+    int res = q1.peak();
+    if (res != -1) {
+        cout << "Front element is: " << res << endl;
+    }
+
+    return 0;
 }
